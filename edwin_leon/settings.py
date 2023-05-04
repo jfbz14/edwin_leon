501780986d58b14df192746197263b16a5d9e61e
Jhon Builes <83386176+jfbz14@users.noreply.github.com>
2023-05-04 10:30:42 -0500

    update form template

--- edwin_leon/settings.py
+++ edwin_leon/settings.py
@@ -86,14 +86,16 @@ DATABASES = {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+}
+
 """
 
 DATABASES = {
 'default': dj_database_url.config(
     default=env('DATABASE_URL')
     )
-}  
+} 
+ 
 
 
 # Password validation
